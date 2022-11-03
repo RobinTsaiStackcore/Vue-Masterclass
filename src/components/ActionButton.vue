0b3ba83077d1195130454baf3ce810f0b62ebfe3
Robin Tsai <robin.tsai@stackcore.com>
2022-11-03 15:41:41 +0800

    106. Intro to Computed Properties 利用 computed 計算矩形面積

---
+++ src/components/ActionButton.vue
@@ -0,0 +1,45 @@
+<template>
+  <button :class="{ primary: primary }">
+    {{ text }}
+  </button>
+
+  <button
+    :class="{ primary: primary }"
+    @click="doubleHeight"
+  >
+    {{ area }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "ActionButton",
+  props: ["text"],
+  data() {
+    return {
+      primary: true,
+      width: 10,
+      height: 5,
+    };
+  },
+  computed: {
+    area() {
+      return this.width * this.height;
+    },
+  },
+  methods: {
+    doubleHeight() {
+      this.height = this.height * 2;
+    },
+  },
+};
+</script>
+
+<style scoped>
+button {
+  @apply px-5 py-3 rounded font-medium;
+}
+.primary {
+  @apply text-white bg-brand-blue-1 hover:shadow-blue;
+}
+</style>
