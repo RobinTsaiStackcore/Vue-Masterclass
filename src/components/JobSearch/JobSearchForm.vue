53497e1854614adb9e82f1c662f72003f2e9d24c
STACKCORE\robin.tsai <robin.tsai@stackcore.com>
2022-11-07 17:56:00 +0800

    184. Pass in the value prop in JobSearchForm

--- src/components/JobSearch/JobSearchForm.vue
+++ src/components/JobSearch/JobSearchForm.vue
@@ -10,11 +10,10 @@
     <div class="flex flex-nowrap flex-1 h-full text-base font-light">
       <div class="relative flex items-center flex-1 h-full pr-3">
         <label class="absolute left-0 -top-10">Role</label>
-        <input
-          v-model="role"
-          type="text"
-          placeholder="Software engineer"
-          class="w-full text-lg font-normal focus:outline-none"
+        <text-input
+          placeholder="Search jobs"
+          :value="role"
+          @handle-input="updateRole"
         />
       </div>
 
@@ -25,11 +24,10 @@
 
       <div class="relative flex items-center flex-1 h-full pl-3">
         <label class="absolute left-0 -top-10">Where?</label>
-        <input
-          v-model="location"
-          type="text"
+        <text-input
           placeholder="Los Angeles"
-          class="w-full text-lg font-normal focus:outline-none"
+          :value="location"
+          @handle-input="location = $event"
         />
       </div>
     </div>
@@ -44,17 +42,24 @@
 
 <script>
 import ActionButton from "@/components/Shared/ActionButton.vue";
+import TextInput from "../Shared/TextInput.vue";
 
 export default {
   name: "JobSearchForm",
   components: {
     ActionButton,
+    TextInput,
   },
   data() {
     return {
       role: "",
       location: "",
     };
+  },
+  methods: {
+    updateRole(payload) {
+      this.role = payload;
+    },
   },
 };
 </script>
