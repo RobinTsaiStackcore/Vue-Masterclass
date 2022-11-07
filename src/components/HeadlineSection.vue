a83b48ae7d782f0571b7791b7b4fe1092b5d91fe
STACKCORE\robin.tsai <robin.tsai@stackcore.com>
2022-11-07 11:37:02 +0800

    151. Adding Function to Headline Component

--- src/components/HeadlineSection.vue
+++ src/components/HeadlineSection.vue
@@ -1,11 +1,17 @@
 <template>
   <section>
-    <h1>{{ action }} for everyone</h1>
-    <h2>Find your next job at bobo corp</h2>
+    <h1 class="font-bold tracking-tighter text-8xl mb-14">
+      <span :class="actionClasses">{{ action }}</span>
+      <br />
+      for everyone
+    </h1>
+    <h2 class="text-3xl font-light">Find your next job at bobo corp</h2>
   </section>
 </template>
 
 <script>
+import nextElementInList from "@/utils/nextElementInList";
+
 export default {
   name: "HeadlineSection",
   data() {
@@ -13,6 +19,17 @@ export default {
       action: "Build",
       interval: null,
     };
+  },
+  computed: {
+    actionClasses() {
+      return {
+        // build: this.action === "Build",
+        // create: this.action === "Create",
+        // design: this.action === "Design",
+        // code: this.action === "Code",
+        [this.action.toLowerCase()]: true,
+      };
+    },
   },
   created() {
     this.changeTitle();
@@ -24,12 +41,27 @@ export default {
     changeTitle() {
       this.interval = setInterval(() => {
         const actions = ["Build", "Create", "Design", "Code"];
-        const currentActionIndex = actions.indexOf(this.action);
-        const nextActionIndex = (currentActionIndex + 1) % 4;
-        const nextAction = actions[nextActionIndex];
-        this.action = nextAction;
+        this.action = nextElementInList(actions, this.action);
       }, 1000);
     },
   },
 };
 </script>
+
+<style scoped>
+.build {
+  color: #1a73e8;
+}
+
+.create {
+  color: #34a853;
+}
+
+.design {
+  color: #f9ab00;
+}
+
+.code {
+  color: #d93025;
+}
+</style>
